30ca2aa5ae0306bd39e86c0b529991970ac49add
Pedro Maltez <maltezpedro@gmail.com>
2013-04-26 15:58:21 +0100

    Delete unused migrations and models.

--- app/models/library.rb
+++
@@ -1,6 +0,0 @@
-class Library < ActiveRecord::Base
-  attr_accessible :user, :games
-
-  belongs_to :user
-  has_many :games
-end
--- app/models/opinion.rb
+++
@@ -1,6 +0,0 @@
-class Opinion < ActiveRecord::Base
-  attr_accessible :profile, :game
-
-  belongs_to :profile
-  belongs_to :game
-end
